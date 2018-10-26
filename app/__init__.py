f9ed9fccd468b5caf908d5046d4c9ddd3fe063be
roger254 <rogerbrian.mutua@gmail.com>
2018-10-26 10:58:24 +0300

    Initial Commit

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,7 @@ jwt = JWTManager()
 
 
 def create_app(config_name):
+    """Create app"""
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
